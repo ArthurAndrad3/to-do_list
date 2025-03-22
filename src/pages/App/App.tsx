e0e76516fe898a376e1785a2dc3adf7c191600ca
ArthurAndrad3 <arthur-amf@hotmail.com>
2025-03-22 14:34:07 -0300

    change files

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "../../components/Header/Header";
+import Header from "../../components/Header2/Header2";
 import ToDoList from "../../components/ToDoList/ToDoList";
 import { useState } from "react";
 
